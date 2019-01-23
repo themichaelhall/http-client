ccc0fd5dcadd9b61bd7020edfe4f2325b7c4f1ff
Michael Hall <hall@michaelhall.se>
2019-01-23 19:33:02 +0100

    Add custom request handler tests

---
+++ tests/CustomRequestHandlerTest.php
@@ -0,0 +1,152 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichaelHall\HttpClient\Tests;
+
+use DataTypes\FilePath;
+use DataTypes\Url;
+use MichaelHall\HttpClient\HttpClient;
+use MichaelHall\HttpClient\HttpClientRequest;
+use MichaelHall\HttpClient\Tests\TestFiles\CustomRequestHandler;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Same tests as in HttpClientTest, but with a custom fake request handler.
+ */
+class CustomRequestHandlerTest extends TestCase
+{
+    /**
+     * Test fetching a page with 200 OK response.
+     */
+    public function testOkResponse()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/anything'));
+        $request->addHeader('X-Test-Header: Foo Bar');
+        $response = $client->send($request);
+        $jsonContent = json_decode($response->getContent(), true);
+
+        self::assertSame(200, $response->getHttpCode());
+        self::assertSame('GET', $jsonContent['method']);
+        self::assertSame('Foo Bar', $jsonContent['headers']['X-Test-Header']);
+        self::assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * Test fetching a page with failed connection.
+     */
+    public function testFailedConnectionResponse()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://localhost:123/'));
+        $response = $client->send($request);
+
+        self::assertSame(0, $response->getHttpCode());
+        self::assertSame('Failed to connect to localhost port 123: Connection refused', $response->getContent());
+        self::assertFalse($response->isSuccessful());
+    }
+
+    /**
+     * Test fetching a page with 404 Not Found response.
+     */
+    public function testNotFoundResponse()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/status/404'));
+        $response = $client->send($request);
+
+        self::assertSame(404, $response->getHttpCode());
+        self::assertSame('', $response->getContent());
+        self::assertFalse($response->isSuccessful());
+    }
+
+    /**
+     * Test a POST request.
+     */
+    public function testPostRequest()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/anything'), 'POST');
+        $request->addHeader('Expect: 100-continue');
+        $request->setPostField('Foo', 'Bar');
+        $response = $client->send($request);
+        $jsonContent = json_decode($response->getContent(), true);
+
+        self::assertSame(200, $response->getHttpCode());
+        self::assertSame('POST', $jsonContent['method']);
+        self::assertSame(['Foo' => 'Bar'], $jsonContent['form']);
+        self::assertSame('application/x-www-form-urlencoded', $jsonContent['headers']['Content-Type']);
+        self::assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * Test a POST request with files.
+     */
+    public function testPostRequestWithFiles()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/anything'), 'POST');
+        $request->setFile('Foo', FilePath::parse(__DIR__ . '/TestFiles/hello-world.txt'));
+        $request->setPostField('Bar', 'Baz');
+        $response = $client->send($request);
+        $jsonContent = json_decode($response->getContent(), true);
+
+        self::assertSame(200, $response->getHttpCode());
+        self::assertSame('POST', $jsonContent['method']);
+        self::assertSame(['Foo' => 'Hello World!'], $jsonContent['files']);
+        self::assertSame(['Bar' => 'Baz'], $jsonContent['form']);
+        self::assertStringStartsWith('multipart/form-data', $jsonContent['headers']['Content-Type']);
+        self::assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * Test fetching a page with custom headers.
+     */
+    public function testWithCustomHeaders()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/response-headers?X-Test-Header=Foo'));
+        $response = $client->send($request);
+
+        self::assertSame(200, $response->getHttpCode());
+        self::assertContains('X-Test-Header: Foo', $response->getHeaders());
+        self::assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * Test a PUT request with raw content.
+     */
+    public function testWithRawContent()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/anything'), 'PUT');
+        $request->addHeader('Content-Type: application/json');
+        $request->setRawContent('{"Foo": "Bar"}');
+        $response = $client->send($request);
+        $jsonContent = json_decode($response->getContent(), true);
+
+        self::assertSame(200, $response->getHttpCode());
+        self::assertSame('PUT', $jsonContent['method']);
+        self::assertSame(['Foo' => 'Bar'], $jsonContent['json']);
+        self::assertSame('application/json', $jsonContent['headers']['Content-Type']);
+        self::assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * Test fetching pages with cookies.
+     */
+    public function testWithCookies()
+    {
+        $client = new HttpClient(new CustomRequestHandler());
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/cookies/set?Foo=Bar'));
+        $client->send($request);
+        $request = new HttpClientRequest(Url::parse('https://httpbin.org/cookies'));
+        $response = $client->send($request);
+        $jsonContent = json_decode($response->getContent(), true);
+
+        self::assertSame(200, $response->getHttpCode());
+        self::assertSame(['Foo' => 'Bar'], $jsonContent['cookies']);
+        self::assertTrue($response->isSuccessful());
+    }
+}
---
+++ tests/TestFiles/CustomRequestHandler.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichaelHall\HttpClient\Tests\TestFiles;
+
+use DataTypes\Interfaces\FilePathInterface;
+use MichaelHall\HttpClient\HttpClientRequestInterface;
+use MichaelHall\HttpClient\HttpClientResponse;
+use MichaelHall\HttpClient\HttpClientResponseInterface;
+use MichaelHall\HttpClient\RequestHandlers\RequestHandlerInterface;
+
+/**
+ * A custom request handler.
+ *
+ * A very rough emulation of some of the functionality from https://httpbin.org/.
+ */
+class CustomRequestHandler implements RequestHandlerInterface
+{
+    /**
+     * CustomRequestHandler constructor.
+     */
+    public function __construct()
+    {
+        $this->cookies = [];
+    }
+
+    /**
+     * Handles a request.
+     *
+     * @param HttpClientRequestInterface $request The request.
+     *
+     * @return HttpClientResponseInterface The response.
+     */
+    public function handleRequest(HttpClientRequestInterface $request): HttpClientResponseInterface
+    {
+        if ($request->getUrl()->getPort() !== 443) {
+            return new HttpClientResponse(0, 'Failed to connect to ' . $request->getUrl()->getHost() . ' port ' . $request->getUrl()->getPort() . ': Connection refused');
+        }
+
+        $url = $request->getUrl();
+        $path = $url->getPath();
+
+        $responseCode = 200;
+        $responseHeaders = [];
+        $responseContent = [
+            'method'  => $request->getMethod(),
+            'headers' => [],
+            'form'    => $request->getPostFields(),
+            'files'   => [],
+            'json'    => [],
+            'cookies' => [],
+        ];
+
+        // Headers.
+        foreach ($request->getHeaders() as $requestHeader) {
+            $requestHeaderParts = explode(':', $requestHeader, 2);
+            $responseContent['headers'][$requestHeaderParts[0]] = ltrim($requestHeaderParts[1]);
+        }
+
+        // Special paths.
+        if ($path->getDirectory()->__toString() === '/status/') {
+            $responseCode = intval($path->getFilename());
+        } elseif ($path->__toString() === '/response-headers') {
+            $queryStringParts = explode('=', $url->getQueryString());
+            $responseHeaders[] = $queryStringParts[0] . ': ' . $queryStringParts[1];
+        } elseif ($path->__toString() === '/cookies') {
+            $responseContent['cookies'] = $this->cookies;
+        } elseif ($path->__toString() === '/cookies/set') {
+            $queryStringParts = explode('=', $url->getQueryString());
+            $this->cookies[$queryStringParts[0]] = $queryStringParts[1];
+        }
+
+        // Files.
+        foreach ($request->getFiles() as $name => $requestFile) {
+            /** @var FilePathInterface $requestFile */
+            $responseContent['files'][$name] = file_get_contents($requestFile->__toString());
+        }
+
+        // Raw content.
+        if ($request->getRawContent() !== '') {
+            $responseContent['json'] = json_decode($request->getRawContent(), true);
+        }
+
+        // Content-type.
+        if (!empty($responseContent['files'])) {
+            $responseContent['headers']['Content-Type'] = 'multipart/form-data';
+        } elseif (!empty($responseContent['form'])) {
+            $responseContent['headers']['Content-Type'] = 'application/x-www-form-urlencoded';
+        }
+
+        // Create response.
+        if ($responseCode === 404) {
+            return new HttpClientResponse($responseCode);
+        }
+
+        $response = new HttpClientResponse($responseCode, json_encode($responseContent));
+        foreach ($responseHeaders as $responseHeader) {
+            $response->addHeader($responseHeader);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @var array My cookies.
+     */
+    private $cookies;
+}
